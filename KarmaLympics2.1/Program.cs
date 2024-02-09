3ce50939a1617d50bee1f05868f9bfc02465829b
Cordelia Kolstø <Cordeliakolsto@gmail.com>
2024-02-09 15:52:42 +0100

    Added connectionString to Json and Program builder

--- KarmaLympics2.1/Program.cs
+++ KarmaLympics2.1/Program.cs
@@ -1,4 +1,7 @@
 
+using KarmaLympics2._1.Data;
+using Microsoft.EntityFrameworkCore;
+
 namespace KarmaLympics2._1
 {
     public class Program
@@ -13,6 +16,10 @@ namespace KarmaLympics2._1
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+            builder.Services.AddDbContext<DataContext>(options =>
+            {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+            });
 
             var app = builder.Build();
 
