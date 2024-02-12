ab0226fa1ed35cca511e8968fc491e0c3d67bb35
Cordelia Kolstø <Cordeliakolsto@gmail.com>
2024-02-12 15:02:17 +0100

    Created IchallengeRepository

---
+++ KarmaLympics2.1/Interfaces/IChallangeRepository.cs
@@ -0,0 +1,11 @@
+﻿using KarmaLympics2._1.Models;
+
+namespace KarmaLympics2._1.Interfaces
+{
+    public interface IChallangeRepository
+    {
+        ICollection<Challenge> GetChallenges();
+        Challenge GetChallenge(int id);
+        int GetChallengePoint(int id);
+    }
+}
