cf3f4706ec5ff7ac4670be1fd573dc83bfd08db1
Cordelia Kolstø <Cordeliakolsto@gmail.com>
2024-02-12 15:25:02 +0100

    Created Interface Teamchallenge

---
+++ KarmaLympics2.1/Interfaces/ITeamChallengeRepository.cs
@@ -0,0 +1,12 @@
+﻿using KarmaLympics2._1.Models;
+
+namespace KarmaLympics2._1.Interfaces
+{
+    public interface ITeamChallengeRepository
+    {
+        ICollection<TeamChallenge> GetTeamChallenges();
+        TeamChallenge GetTeamChallenge(int id);
+        TeamChallenge GetTeamAnwser();
+        TeamChallenge GetTeamPointsEarned(int id);
+    }
+}
