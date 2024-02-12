d22a8bd0c69a594c709deeef8aaf9d5738834ca9
Cordelia Kolstø <Cordeliakolsto@gmail.com>
2024-02-12 15:12:52 +0100

    Created ChallengenRepository.

---
+++ KarmaLympics2.1/Repository/ChallengeRepository.cs
@@ -0,0 +1,35 @@
+﻿using KarmaLympics2._1.Data;
+using KarmaLympics2._1.Interfaces;
+using KarmaLympics2._1.Models;
+
+namespace KarmaLympics2._1.Repository
+{
+    public class ChallengeRepository : IChallangeRepository
+    {
+        private readonly DataContext _context;
+
+        public ChallengeRepository(DataContext context)
+        {
+            _context = context;
+        }
+
+        public Challenge GetChallenge(int id)
+        {
+            return _context.Challenges.Where(c => c.Id == id).FirstOrDefault();
+        }
+
+        public int GetChallengePoint(int id)
+        {
+            int challengePoints = _context.Challenges
+                .Where(c => c.Id == id)
+                .Select(c => c.Points)
+                .FirstOrDefault();
+            return challengePoints;
+        }
+
+        public ICollection<Challenge> GetChallenges()
+        {
+            return _context.Challenges.OrderBy(c => c.Id).ToList();  
+        }
+    }
+}
