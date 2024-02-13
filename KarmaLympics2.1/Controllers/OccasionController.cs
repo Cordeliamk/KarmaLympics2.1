a98524d0e23df627d979549c1249a6889d8b880b
Cordelia Kolstø <Cordeliakolsto@gmail.com>
2024-02-13 15:05:22 +0100

    Added a nee OccasionController

---
+++ KarmaLympics2.1/Controllers/OccasionController.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using KarmaLympics2._1.Interfaces;
+using KarmaLympics2._1.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KarmaLympics2._1.Controllers
+{
+    public class OccasionController(IOccasionRepository occasionRepository, IMapper mapper) : ControllerBase
+    {
+        private readonly IOccasionRepository _occasionRepository = occasionRepository;
+        private readonly IMapper _mapper = mapper;
+
+
+    }
+}
