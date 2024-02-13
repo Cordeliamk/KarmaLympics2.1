5c2df1d1585fcf989ccf599f8946c5976a243d73
Cordelia Kolstø <Cordeliakolsto@gmail.com>
2024-02-13 15:46:59 +0100

    Created methods in OccasionController

--- KarmaLympics2.1/Controllers/OccasionController.cs
+++ KarmaLympics2.1/Controllers/OccasionController.cs
@@ -1,7 +1,10 @@
 ﻿using AutoMapper;
+using KarmaLympics2._1.Dto;
 using KarmaLympics2._1.Interfaces;
+using KarmaLympics2._1.Models;
 using KarmaLympics2._1.Repository;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace KarmaLympics2._1.Controllers
 {
@@ -10,6 +13,50 @@ namespace KarmaLympics2._1.Controllers
         private readonly IOccasionRepository _occasionRepository = occasionRepository;
         private readonly IMapper _mapper = mapper;
 
+        [HttpGet]
+        [ProducesResponseType(200, Type = typeof(IEnumerable<Occasion>))]
 
+        public IActionResult GetOccasion(int occasionId)
+        {
+            if (!_occasionRepository.OccasionExists(occasionId))
+                return NotFound();
+
+            OccasionDto occasion = _mapper.Map<OccasionDto>(_occasionRepository.GetOccasion(occasionId));
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+            return Ok(occasion);
+        }
+
+        [HttpGet("{occasionId}/hostMail")]
+        [ProducesResponseType(200, Type = typeof(string))]
+        [ProducesResponseType(400)]
+        public IActionResult GetOccasionHostMail(int occasionId)
+        {
+            if (!_occasionRepository.OccasionExists(occasionId))
+                return NotFound();
+
+            string hostMail = _occasionRepository.GetOccasionHostMail(occasionId);
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            return Ok(hostMail);
+        }
+
+        [HttpGet("{occasionId}/OccasionUrl")]
+        [ProducesResponseType(200, Type = typeof(string))]
+        [ProducesResponseType(400)]
+        public IActionResult GetOccasionUrl(int occasionId)
+        {
+            if (!_occasionRepository.OccasionExists(occasionId))
+                return NotFound();
+
+            string occasionUrl = _occasionRepository.GetOccasionHostMail(occasionId);
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            return Ok(occasionUrl);
+        }
     }
 }
